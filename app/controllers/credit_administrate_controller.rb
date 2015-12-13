761cbcdda282133030eda4c280a9d22b8d31c32d
Pikachoo <novakovsk@gmail.com>
2015-12-14 00:46:04 +0300

    after applyment_confirmation create account for with credit

--- app/controllers/credit_administrate_controller.rb
+++ app/controllers/credit_administrate_controller.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 class CreditAdministrateController < ApplicationController
   before_filter :check_is_manage
 
@@ -22,17 +25,32 @@ class CreditAdministrateController < ApplicationController
 
   def close
     ClientCredit.find(params[:id]).update_attributes(credit_state: 3)
-    return redirect_to credit_administrate_index_path
+    redirect_to credit_administrate_index_path
   end
 
   def applyment_confirmation
-    ClientCredit.find(params[:id]).update_attributes(credit_state: 1, begin_date: Date.today)
-    return redirect_to credit_administrate_index_path
+    client_credit = ClientCredit.find(params[:id])
+    # client_credit.update_attributes(credit_state: 1, begin_date: Date.today)
+    create_account(client_credit)
+     redirect_to credit_administrate_index_path
   end
 
   def applyment_decline
     ClientCredit.find(params[:id]).update_attributes(credit_state: 4)
-    return redirect_to credit_administrate_index_path
+    redirect_to credit_administrate_index_path
   end
+
+  def create_account(client_credit)
+    uri = URI.parse("http://bank.tarnenok.by/api/accounts/open?typeId=1&currencyId=#{client_credit.credit.currency_id}&clientId=#{client_credit.client.id}&sum=#{client_credit.sum}")
+    req = Net::HTTP::Post.new(uri.request_uri)
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+
+    account_hash = JSON.parse(res.body)
+    account = Account.find(account_hash['id'])
+    account.update_attributes(credit_id: client_credit.id)
+  end
+
 end
 
